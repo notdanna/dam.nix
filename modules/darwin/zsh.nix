191166dd54ac53528ea7bd3a40f3bc56d5a7218f
notdanna <afrancor1900@alumno.ipn.mx>
2026-04-01 22:23:51 -0600

    cgange directory

--- modules/darwin/zsh.nix
+++ modules/darwin/zsh.nix
@@ -1,6 +1,6 @@
 { ... }: {
   programs.zsh.shellAliases = {
-    nxb = "sudo darwin-rebuild switch --flake ~/.config/nix#dam";
+    nxb = "sudo darwin-rebuild switch --flake ~/dam.nix#dam";
     nxu = "nix flake update ~/.config/nix && sudo darwin-rebuild switch --flake ~/.config/nix#dam";
   };
 }
